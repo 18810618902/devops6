5a827400f94082d21496b07f4411ea4ba1aced93
ly <>
2017-12-28 13:52:00 +0800

    update

--- lesson1/liaoyao/devops/devops/urls.py
+++ lesson1/liaoyao/devops/devops/urls.py
@@ -19,4 +19,6 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^login$',views.needlogin,name='login'),
+    url(r'^test$', views.test,name='test'),
 ]
--- lesson1/liaoyao/devops/devops/views.py
+++ lesson1/liaoyao/devops/devops/views.py
@@ -1,8 +1,38 @@
 #encoding: utf-8
 from django.shortcuts import render
-
-
+from django.http import HttpResponse,JsonResponse
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
     return render(request,'index.html')
+
+
+def needlogin(request):
+    print(request.POST)
+    if request.method == 'GET':
+        return render(request, 'pages/examples/login.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        pwd = request.POST.get('pwd')
+        user = authenticate(username=name,password=pwd)
+        result = {}
+        if user:
+            login(request,user)
+            result['status'] = 0
+            #return HttpResponseRedirect('/')
+        else:
+            result['status'] = 1
+        return JsonResponse(result)
+
+
+
+def test(request):
+    print(request.POST)
+    if request.method == 'GET':
+        return render(request, 'test.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        pwd = request.POST.get('pwd')
+        result = {name: pwd}
+        return JsonResponse(result)
