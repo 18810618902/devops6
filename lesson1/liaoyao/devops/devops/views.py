3c66c87d3a6ae407a16f58590485f140c55e7890
ly <>
2017-12-28 18:10:56 +0800

    update

--- lesson1/liaoyao/devops/devops/views.py
+++ lesson1/liaoyao/devops/devops/views.py
@@ -2,8 +2,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request):
     return render(request,'index.html')
 
