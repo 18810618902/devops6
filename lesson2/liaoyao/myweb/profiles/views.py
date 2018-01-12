165e9f9bfab4648f2a707e8c578a3d791c55d17d
ly <>
2018-01-12 09:44:27 +0800

    update

--- lesson2/liaoyao/myweb/profiles/views.py
+++ lesson2/liaoyao/myweb/profiles/views.py
@@ -4,14 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.contrib.auth.models import User
-# Create your views here.
-
 
 
 def index(request):
     return HttpResponse('index')
-
-
 
 def userinfo(request,user):
     result = dict()
@@ -19,14 +15,12 @@ def userinfo(request,user):
     result['msg'] = 'userdata'
     try:
         res = User.objects.get(username=user)
-        result['data']['address'] = res.userprofile.address.encode('utf-8')
+        result['data']['address'] = res.userprofile.address
         result['data']['phone'] = res.userprofile.phone
         result['data']['id'] = res.id
         result['data']['username'] = res.username
         result['status'] = 0
     except User.DoesNotExist:
-        print('user is no exist')
-        result['data'] = 'None'
+        result['data'] = 'user is no exist'
         result['status'] = 1
-    print(result)
     return JsonResponse(result)
