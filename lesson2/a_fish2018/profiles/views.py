785926f2c04ed68c18420f039eca9f1d130900f8
xiazy <xiazy@chiyuchengdeMacBook-Air.local>
2018-01-15 17:59:56 +0800

    fish2018

---
+++ lesson2/a_fish2018/profiles/views.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from .models import *
+from django.shortcuts import render
+from django.http import  HttpResponse,JsonResponse,HttpResponseRedirect
+from django.views import View
+from django.views.generic import TemplateView
+# Create your views here.
+
+
